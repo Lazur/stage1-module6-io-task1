598a27ae98a15c2be18ac93635dce4c8003610e7
Kyrylo_Popov <kyrylo_popov@epam.com>
2023-07-17 23:18:33 +0300

    Staga1 task 1 IO

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileReader {
+    
+    public Profile getDataFromFile(File file) {    
+        String name = "";
+        Integer age = 1;
+        String email = "";
+        Long phone = (long) 1;
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                String[] data = line.split(":");
+                if (line.startsWith("Name:")) {
+                    name = data[1].trim();    
+                } else if (line.startsWith("Age:")) {
+                    age = Integer.parseInt(data[1].trim());
+                } else if (line.startsWith("Email:")) {
+                    email = data[1].trim();
+                } else if (line.startsWith("Phone:")) {
+                    phone = Long.parseLong(data[1].trim());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return new Profile(name, age, email, phone);
     }
 }
